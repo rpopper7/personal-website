15e6861b29880f3a9ef464e599e82db74fad1ca3
Rebecca Popper <rpopper7@users.noreply.github.com>
2025-11-02 18:30:16 -0500

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-900`}>
         {children}
       </body>
     </html>
