c4aca05dde6cef7d02475e28870e59e58ed9d73e
Rebecca Popper <rpopper7@users.noreply.github.com>
2025-11-03 21:56:03 -0500

    link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { TypeAnimation} from "react-type-animation";
-import { FaLinkedin, FaGithub, FaFileAlt } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import {FaLinkedin, FaGithub, FaFileAlt, FaArrowRight} from "react-icons/fa";
 
 export default function Home() {
     return (
@@ -14,8 +14,17 @@ export default function Home() {
                     cursor={true}
                     className="text-4xl font-bold mb-4 min-w-[350px]"
                 />
-                <p className="text-lg mb-6">
-                    This is my personal website.
+                <p className="text-lg mb-6 inline-flex gap-2">
+                    Software Engineer at
+                    <a
+                        href="https://www.mayoclinic.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="font-bold underline hover:text-blue-600 inline-flex items-center group"
+                    >
+                        Mayo Clinic
+                        <FaArrowRight className="text-sm -rotate-45 group-hover:translate-x-1 group-hover:-translate-y-1 transition-transform" />
+                    </a>
                 </p>
 
                 <div className="flex gap-4 items-center">
@@ -24,10 +33,10 @@ export default function Home() {
                         <FaLinkedin size={24}/>
                     </a>
                     <a href="https://github.com/rpopper7" target="_blank" rel="noopener noreferrer"
-                       className="hover:text-gray-600">
+                       className="hover:text-blue-600">
                         <FaGithub size={24}/>
                     </a>
-                    <a href="/resume.pdf" target="_blank" className="flex items-center gap-2 hover:text-gray-600">
+                    <a href="/resume.pdf" target="_blank" className="flex items-center gap-2 hover:text-blue-600">
                         <FaFileAlt size={24}/>
                         <span>Resume</span>
                     </a>
